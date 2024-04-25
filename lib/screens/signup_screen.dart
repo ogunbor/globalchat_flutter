25d093c616e623b72041a12acda51d63fdd689f2
mompei <ogunborjerry@gmail.com>
2024-04-25 23:25:16 +0100

    Modify: removed the controllers part from thr signup_screen as it is no longer needed there

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -17,25 +17,6 @@ class _SignupScreenState extends State<SignupScreen> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
 
-  Future<void> createAccount() async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email.text, password: password.text);
-
-      Navigator.pushAndRemoveUntil(context,
-          MaterialPageRoute(builder: (context) {
-        return DashboardScreen();
-      }), (route) {
-        return false;
-      });
-    } catch (e) {
-      SnackBar messageSnackbar =
-          SnackBar(backgroundColor: Colors.red, content: Text(e.toString()));
-
-      ScaffoldMessenger.of(context).showSnackBar(messageSnackbar);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
