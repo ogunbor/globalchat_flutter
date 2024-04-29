8ac5f6381492c5f7f5408ccc3dd18cca8cb0c65b
mompei <ogunborjerry@gmail.com>
2024-04-29 12:12:46 +0100

    Did background work on the profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -25,14 +25,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
+        title: Text(""),
       ),
-      body: Column(
-        children: [
-          Text(userProvider.userName),
-          Text(userProvider.userEmail),
-          Text(userProvider.userId)
-        ],
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: Text(userProvider.userName[0]),
+            ),
+            SizedBox(height: 8),
+            Text(
+              userProvider.userName,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(userProvider.userEmail),
+            ElevatedButton(onPressed: () {}, child: Text("Edit Profile"))
+          ],
+        ),
       ),
     );
   }
