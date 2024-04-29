718bba2d7fe694dab71417ee7a4d033cb22b668b
mompei <ogunborjerry@gmail.com>
2024-04-29 12:03:55 +0100

    Changed default hamburger icon and also brought in user profile unto the sidebar using Provider

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:globalchat/providers/userProvider.dart';
 import 'package:globalchat/screens/profile_screen.dart';
 import 'package:globalchat/screens/splash_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -14,6 +16,7 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   var user = FirebaseAuth.instance.currentUser;
   var db = FirebaseFirestore.instance;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
 
   List<Map<String, dynamic>> chatroomsList = [];
 
@@ -36,15 +39,44 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var userProvider = Provider.of<UserProvider>(
+        context); // Provider is being imported since we need the profile here also
     return Scaffold(
+        key: scaffoldKey,
         appBar: AppBar(
           title: Text('Global Chat'),
+          leading: InkWell(
+            onTap: () {
+              scaffoldKey.currentState!.openDrawer();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: CircleAvatar(
+                  radius: 30, child: Text(userProvider.userName[0])),
+            ),
+          ),
         ),
         drawer: Drawer(
           child: Container(
             child: Column(
               children: [
                 SizedBox(height: 50),
+                ListTile(
+                  onTap: () async {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return ProfileScreen();
+                    }));
+                  },
+                  leading: CircleAvatar(
+                    child: Text(userProvider.userName[0]),
+                  ),
+                  title: Text(
+                    userProvider.userName,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(userProvider.userEmail),
+                ),
                 ListTile(
                   onTap: () async {
                     await FirebaseAuth.instance.signOut();
@@ -56,18 +88,21 @@ class _DashboardScreenState extends State<DashboardScreen> {
                     });
                   },
                   leading: Icon(Icons.logout),
-                  title: Text("logout"),
+                  title: Text("Profile"),
                 ),
                 ListTile(
                   onTap: () async {
-                    Navigator.push(context,
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.pushAndRemoveUntil(context,
                         MaterialPageRoute(builder: (context) {
-                      return ProfileScreen();
-                    }));
+                      return SplashScreen();
+                    }), (route) {
+                      return false;
+                    });
                   },
-                  leading: Icon(Icons.people),
-                  title: Text("profile"),
-                )
+                  leading: Icon(Icons.logout),
+                  title: Text("Logout"),
+                ),
               ],
             ),
           ),
@@ -78,7 +113,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
               String chatroomName = chatroomsList[index]["chatroom_name"] ?? "";
               return ListTile(
                 leading: CircleAvatar(
-                  child: Text(chatroomName[0]),
+                  backgroundColor: Colors.blueGrey[900],
+                  child: Text(chatroomName[0],
+                      style: TextStyle(color: Colors.white)),
                 ),
                 title: Text(chatroomName),
                 subtitle: Text(
