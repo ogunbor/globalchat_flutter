95649e1c618a3f9ce4902cead7e57a708621de3f
mompei <ogunborjerry@gmail.com>
2024-04-30 06:18:25 +0100

    Send chat messages (data) into chatroom (database)

--- lib/screens/chatroom_screen.dart
+++ lib/screens/chatroom_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:globalchat/providers/userProvider.dart';
+import 'package:provider/provider.dart';
 
 class ChatroomScreen extends StatefulWidget {
   //To pass the title from dashboard screen unto the chatroom screen
@@ -13,6 +16,25 @@ class ChatroomScreen extends StatefulWidget {
 }
 
 class _ChatroomScreenState extends State<ChatroomScreen> {
+  var db = FirebaseFirestore.instance;
+  TextEditingController messageText = TextEditingController();
+
+  Future<void> sendMessage() async {
+    if (messageText.text.isEmpty) {
+      return;
+    }
+    Map<String, dynamic> messageToSend = {
+      "text": messageText.text,
+      "sender_name": Provider.of<UserProvider>(context, listen: false).userName,
+      "chatroom_id": widget.chatroomId,
+      "timestamp": FieldValue.serverTimestamp()
+    };
+    try {
+      await db.collection("messages").add(messageToSend);
+    } catch (e) {}
+    messageText.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,14 +43,29 @@ class _ChatroomScreenState extends State<ChatroomScreen> {
         children: [
           Expanded(
             child: Container(
-              color: Colors.red,
+              color: Colors.white,
             ),
           ),
           Container(
+            color: Colors.grey[200],
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
-                children: [Expanded(child: TextField()), Icon(Icons.send)],
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: messageText,
+                    decoration: InputDecoration(
+                      hintText: "Write message here...",
+                      border: InputBorder.none,
+                    ),
+                  )),
+                  InkWell(
+                      onTap: () {
+                        sendMessage();
+                      },
+                      child: Icon(Icons.send))
+                ],
               ),
             ),
           )
